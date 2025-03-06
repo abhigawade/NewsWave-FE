6e82d644588bcc6f0fc2b16fe42837dabb1b84e2
Abhishek-Gawade-1 <abhishek@metamultiples.in>
2025-03-06 14:34:08 +0530

    updaed vite config

--- NewsWave/vite.config.js
+++ NewsWave/vite.config.js
@@ -1,13 +1,30 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path';
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import path from 'path';
 
-// https://vite.dev/config/
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(__dirname, "./src"),
+//     },
+//   },
+// })
+
+
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import * as path from 'node:path';
+
+// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/NewsWave/',  // Set base path for GitHub Pages deployment
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+});
